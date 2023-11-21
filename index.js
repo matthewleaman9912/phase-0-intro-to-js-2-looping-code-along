4e53d77339b5bed0176067d489e3c0620e947fe2
MatthewLeaman <leaman.30@osu.edu>
2023-11-20 21:43:19 -0500

    complete loop lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const stringNames = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "surprise";
+
+function writeCards(stringNames, eventName) {
+    const newArray = [];
+    for(let i = 0; i < stringNames.length; i++) {
+        newArray.push("Thank you, " + stringNames[i] + ", for the wonderful " + eventName + " gift!");
+    }
+    return newArray;
+}
+
+function countDown(number) {
+    let i = number;
+    while (i > 0 - 1) {
+        console.log(i);
+        i--;
+    }
+}
